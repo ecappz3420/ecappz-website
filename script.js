fdef6bf839916190987223b42ec0c6d12385f18a
ECAPPZ <168025592+ecappz3420@users.noreply.github.com>
2024-06-27 20:23:09 +0530

    Add: Added policies

--- script.js
+++ script.js
@@ -1,54 +1,40 @@
-
-
-const sendEmail = async () => {
-    const company = document.querySelector("#company-name").value;
-    const name = document.querySelector("#name").value;
-    const email = document.querySelector("#email").value;
-    const phone = document.querySelector("#phone").value;
-    const note = document.querySelector("#description").value;
-    const url = "https://api.brevo.com/v3/smtp/email";
-    const apiKey = window.API_KEY ;
-
-    const sender = {
-        name: 'ECAPPZ',
-        email: 'sales@ecappz.in'
-    };
-
-    const recipient = {
-        email: 'info@ecappz.in',
-        name: 'John Doe'
-    };
-
-    const emailData = {
-        sender,
-        to: [recipient],
-        subject: 'New Lead',
-        htmlContent: `<html><head></head><body><p>Hi Folks, <br>
-        We have a new lead to track. Here is the details<br>
-        Compay Name: ${company}<br><br>
-        Compay Name: ${name}<br><br>
-        Compay Name: ${email}<br><br>
-        Compay Name: ${phone}<br><br>
-        Compay Name: ${note}<br><br>
-        </p></body></html>`
-    };
-
-    const options = {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            "api-key" :  apiKey
-        },
-        body: JSON.stringify(emailData)
-    };
-
-    fetch(url, options)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
+const showHideFunction = () => {
+    const hash = window.location.hash.replace("#", "");
+    const sections = document.querySelectorAll("section");
+    if (hash === "refund-policy") {
+        sections.forEach(section => {
+            section.style.display = "none";
         })
-        .catch(error => {
-        });
+        document.querySelector(`#${hash}`).style.display = "block";
+        scrollTop();
+    }
+    else if(hash === "general-policy"){
+        sections.forEach(section => {
+            section.style.display = "none";
+        })
+        document.querySelector(`#${hash}`).style.display = "block";  
+        scrollTop(); 
+    }
+    else if(hash === "privacy-policy"){
+        sections.forEach(section => {
+            section.style.display = "none";
+        })
+        document.querySelector(`#${hash}`).style.display = "block";  
+        scrollTop(); 
+    }
+    else {
+        sections.forEach(section=>{
+            (section.id === "general-policy" || section.id === "refund-policy" || section.id === "privacy-policy") ? section.style.display = "none": section.style.display = "block";
+        })
+    }
 }
-document.querySelector("#submit").addEventListener("click", sendEmail);
+
+const scrollTop =()=>{
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
+document.addEventListener("DOMContentLoaded", showHideFunction)
+
+window.addEventListener("hashchange",showHideFunction);
