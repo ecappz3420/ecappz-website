225e1c44399ebb8fba001b9a6b80cdca8f255a62
ecappz3420 <info@ecappz.in>
2024-07-05 17:27:48 +0530

    Git Link

---
+++ script.js
@@ -0,0 +1,40 @@
+const showHideFunction = () => {
+    const hash = window.location.hash.replace("#", "");
+    const sections = document.querySelectorAll("section");
+    if (hash === "refund-policy") {
+        sections.forEach(section => {
+            section.style.display = "none";
+        })
+        document.querySelector(`#${hash}`).style.display = "block";
+        scrollTop();
+    }
+    else if(hash === "general-policy"){
+        sections.forEach(section => {
+            section.style.display = "none";
+        })
+        document.querySelector(`#${hash}`).style.display = "block";  
+        scrollTop(); 
+    }
+    else if(hash === "privacy-policy"){
+        sections.forEach(section => {
+            section.style.display = "none";
+        })
+        document.querySelector(`#${hash}`).style.display = "block";  
+        scrollTop(); 
+    }
+    else {
+        sections.forEach(section=>{
+            (section.id === "general-policy" || section.id === "refund-policy" || section.id === "privacy-policy") ? section.style.display = "none": section.style.display = "block";
+        })
+    }
+}
+
+const scrollTop =()=>{
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
+document.addEventListener("DOMContentLoaded", showHideFunction)
+
+window.addEventListener("hashchange",showHideFunction);
